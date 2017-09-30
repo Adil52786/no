c347268ae9ed2a7f2b36ef2ff7eafb156b8b465d
Dmitry Ponyatov <dponyatov@gmail.com>
2017-09-30 14:32:53 +0400

    + operators + EOL as statement delimiter * T:H without <>
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -12,7 +12,7 @@ string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
 	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
 		S += (*it)->dump(depth+1);
 	return S; }
-string Sym::head() { return "<"+tag+":"+val+">"; }
+string Sym::head() { return tag+":"+val; }
 string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S += '\t'; return S; }
 
 Str::Str(string V):Sym("str",V){}
